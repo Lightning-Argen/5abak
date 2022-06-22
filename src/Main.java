6d6795fbe7b63196ae98aa249092fae8f8e78b1a
асер <animesao06@gmail.com>
2022-06-22 23:22:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        /*System.out.println(25.5 % 5);*/
+
+//        System.out.println(Math.sqrt(81));
+//        double a  = Math.sqrt(16);
+//        System.out.println(a);
+       /* double a = Math.pow(100 ,5);
+       System.out.println(a);*/
+//        System.out.println(Math.floor(4.5));
+//        System.out.println(Math.ceil(4.5));
+//        System.out.println(Math.round(4.5));
+
+        /*Random random = new Random();
+        int a = random.nextInt(20);
+        System.out.println(a);*/
+/*
+        String a = "salam ";
+        String b = "зкшмуе";
+        int c = 25;
+        System.out.println(a + ""+ c + b);*/
+       /* double a = 4.666666;
+int b = (int) a;
+        System.out.println(b);*/
+                 /*int a = 5 ;
+        String c1 = "Salam";
+        System.out.println(c1 +"5"+ a);
+
+
+*/
+
+/*Random random = new Random();
+int a = random.nextInt(1000);
+       System.out.println(a);
+        int c = a % 10;
+        System.out.println(c);
+        int b = a / 10 % 10;
+        System.out.println(b);
+        int n = a / 10 /10 %10;
+        System.out.println(n);
+        System.out.println(c + b + n);*/
+
+       /* Scanner scanner =new Scanner(System.in);
+        int saat = scanner.nextInt();
+        int min = scanner.nextInt();;
+        int sek = scanner.nextInt();;
+        System.out.println(saat*3600+min*60+sek*1);*/
+
+
+
+
+
+    }
+}
